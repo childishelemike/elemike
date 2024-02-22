4434b0b913d058478291b0c9462953e633e10c57
childishelemike <elemike2004@gmail.com>
2024-02-21 23:15:56 -0500

    agregando seccion footer y sus images

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
         </header>
       </article>
       <Title/>
+      <Footer/>
     </>
   )
 }
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,15 +1,24 @@
 import './Footer.css'
+import balon from './assets/basketball-outline.svg' 
+import ig from './assets/logo-instagram.svg' 
+import github from './assets/logo-github.svg' 
 
 export function Footer() {
     return(
         <footer className='Footer__Article'>
-            <span></span>
-            <h1></h1>
+            <div className='Footer__Article--line'></div>
+            <h1>ELEMIKE</h1>
             <div className='Footer__Article--div'>
                 <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                    <li>
+                        <a href='https://www.instagram.com/elemikesunny/'>
+                        <img src={ig} alt='logo-instagram'/>
+                        </a>
+                    </li>
+
+                    <li><a href='https://github.com/childishelemike'><img src={github}/></a></li> 
+                    <li><a href='https://www.youtube.com/watch?v=4TYv2PhG89A'><img src={balon}/></a></li> 
+                    
                 </ul>
             </div>
             <div className='Footer__Article--end'>
