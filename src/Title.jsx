092647178ee2aa0c0d16e41886d62785ef866300
childishelemike <elemike2004@gmail.com>
2024-02-29 18:51:46 -0500

    Modificando seccion de main para tablets

--- src/Title.jsx
+++ src/Title.jsx
@@ -6,11 +6,11 @@ export function Title() {
             <main className="Main__Article">
                 <div className="Main__Article--div">
                     <h1>ELEMIKE</h1>
-                    <p>Marzo 2024</p>
+                    <h3>Marzo 2024</h3>
+                    <p>Gracias a todas las personas que esperaron este momento...</p>
                 </div>
 
                 <div className='Main__Article--About'>
-                    <p>Gracias a todas las personas que esperaron este momento...</p>
                     <p>Hablemos de la sensación que sientes cuando escuchas ritmos nuevos, ritmos que te sacan de tu zona de confort pero aún así te interesan.Este será un proyecto que diariamente te recomendará canciones adecuadas a tu estilo de vida. Además, como es de costumbre en la casa, semanalmente seleccionaremos un artista en particular. Narraremos su historia, sus influencias, premios, álbumes, controversias y muchísimas cosas más.</p>
                     <p>Me presento, mi nombre es Michael Steven Cruz Torres, más conocido como Elemike. Soy estudiante de programación y mi objetivo es combinar mis dos pasiones: la carrera ya mencionada y la música. Me encantaría compartir mis conocimientos y experiencias contigo.  </p>
                 </div>
