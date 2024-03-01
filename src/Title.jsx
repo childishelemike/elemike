8e5c2e55ed9f83caaa484b56f6536fda17eecfe6
childishelemike <elemike2004@gmail.com>
2024-02-29 20:15:45 -0500

    agregando estilos a desktop

--- src/Title.jsx
+++ src/Title.jsx
@@ -12,7 +12,7 @@ export function Title() {
 
                 <div className='Main__Article--About'>
                     <p>Hablemos de la sensación que sientes cuando escuchas ritmos nuevos, ritmos que te sacan de tu zona de confort pero aún así te interesan.Este será un proyecto que diariamente te recomendará canciones adecuadas a tu estilo de vida. Además, como es de costumbre en la casa, semanalmente seleccionaremos un artista en particular. Narraremos su historia, sus influencias, premios, álbumes, controversias y muchísimas cosas más.</p>
-                    <p>Me presento, mi nombre es Michael Steven Cruz Torres, más conocido como Elemike. Soy estudiante de programación y mi objetivo es combinar mis dos pasiones: la carrera ya mencionada y la música. Me encantaría compartir mis conocimientos y experiencias contigo.  </p>
+                    <p>Me presento, mi nombre es Michael Steven Cruz Torres, más conocido como elemike. Soy estudiante de programación y mi objetivo es combinar mis dos pasiones: la carrera ya mencionada y la música. Me encantaría compartir mis conocimientos y experiencias contigo.  </p>
                 </div>
             </main>
         </article>
