ff848a520c588a1f537378224692fa6c7cf8674e
childishelemike <elemike2004@gmail.com>
2024-03-11 15:10:26 -0500

    Package configurado y probando framer-motion

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,39 +2,48 @@ import './Footer.css'
 import music from './assets/musical-notes-outline.svg'
 import ig from './assets/logo-instagram.svg'
 import github from './assets/logo-github.svg'
+import { motion, MotionConfig } from 'framer-motion'
+
+
 
 export function Footer() {
+    const initial = { opacity: 0, y: 60 };
+    const whileInView = { opacity: 1, y: 0, transition: { delay: 0.2, duration: 0.8 } };
+    const viewport = { once: false, amount: 0.5 };
+
     return (
-        <footer className='Footer__Article'>
-            <div className='Footer__Article--line'></div>
-            <article className='Footer__secondArticle'>
-                <div className='Footer__Article--div'>
-                    <h1 className='Footer__Article--h1'>ELEMIKE</h1>
-                    <ul>
-                        <li><a href='https://www.instagram.com/elemikesunny/' aria-label='My Personal Instagram'><img src={ig} alt='logo-instagram' /></a></li>
-                        <li><a href='https://github.com/childishelemike' aria-label='Personal Git'><img src={github} alt='Github logo' /></a></li>
-                        <li><a href='https://www.youtube.com/watch?v=8aVRl5XG-Bs' aria-label='My Favorite song'><img src={music} alt='Music logo' /></a></li>
+        <MotionConfig reducedMotion='user'>
+            <footer className='Footer__Article'>
+                <div className='Footer__Article--line'></div>
+                <article className='Footer__secondArticle'>
+                    <motion.div className='Footer__Article--div' initial={initial} whileInView={whileInView} viewport={viewport}>
+                        <h1 className='Footer__Article--h1'>ELEMIKE</h1>
+                        <ul>
+                            <li><a href='https://www.instagram.com/elemikesunny/' aria-label='My Personal Instagram'><img src={ig} alt='logo-instagram' /></a></li>
+                            <li><a href='https://github.com/childishelemike' aria-label='Personal Git'><img src={github} alt='Github logo' /></a></li>
+                            <li><a href='https://www.youtube.com/watch?v=8aVRl5XG-Bs' aria-label='My Favorite song'><img src={music} alt='Music logo' /></a></li>
 
-                    </ul>
-                </div>
+                        </ul>
+                    </motion.div>
 
-                <div className='Footer__Article--end'>
-                    <div className='Footer__Article--published'>
-                        <h6 className='Footer__Article--h6'>Publicado</h6>
-                        <p>Edición N°23</p>
-                        <p>Marzo 2024</p>
+                    <div className='Footer__Article--end'>
+                        <motion.div className='Footer__Article--published' initial={initial} whileInView={whileInView} viewport={viewport}>
+                            <h6 className='Footer__Article--h6'>Publicado</h6>
+                            <p>Edición N°23</p>
+                            <p>Marzo 2024</p>
+                        </motion.div>
+                        <motion.div className='Footer__Article--editors' initial={initial} whileInView={whileInView} viewport={viewport}>
+                            <h6 className='Footer__Article--h6'>Dedicado a:</h6>
+                            <p>Dilia Torres</p>
+                            <p>Andrea Torres</p>
+                            <p>Paula Sosa</p>
+                            <p>Mafe Rubiano</p>
+                        </motion.div>
                     </div>
-                    <div className='Footer__Article--editors'>
-                        <h6 className='Footer__Article--h6'>Dedicado a:</h6>
-                        <p>Dilia Torres</p>
-                        <p>Andrea Torres</p>
-                        <p>Paula Sosa</p>
-                        <p>Mafe Rubiano</p>
-                    </div>
-                </div>
-            </article>
+                </article>
 
 
-        </footer>
+            </footer>
+        </MotionConfig>
     )
 }
--- src/Title.jsx
+++ src/Title.jsx
@@ -1,20 +1,35 @@
 import './Title.css'
-
+import { motion, MotionConfig } from 'framer-motion'
 export function Title() {
-    return (
-        <article>
-            <main className="Main__Article">
-                <div className="Main__Article--div">
-                    <h1>ELEMIKE</h1>
-                    <h2>Marzo 2024</h2>
-                    <p>Gracias a todas las personas que esperaron este momento...</p>
-                </div>
+    const initial = { opacity: 0, y: 50 };
+    const whileInView = { opacity: 1, y: 0, transition: { delay: 0.2, duration: 0.5 } };
+    const viewport = { once: false, amount: 0.5 };
 
-                <div className='Main__Article--About'>
-                    <p>Hablemos de la sensación que sientes cuando escuchas ritmos nuevos, ritmos que te sacan de tu zona de confort pero aún así te interesan.Este será un proyecto que diariamente te recomendará canciones adecuadas a tu estilo de vida. Además, como es de costumbre en la casa, semanalmente seleccionaremos un artista en particular. Narraremos su historia, sus influencias, premios, álbumes, controversias y muchísimas cosas más.</p>
-                    <p>Me presento, mi nombre es Michael Steven Cruz Torres, más conocido como elemike. Soy estudiante de programación y mi objetivo es combinar mis dos pasiones: la carrera ya mencionada y la música. Me encantaría compartir mis conocimientos y experiencias contigo.  </p>
-                </div>
-            </main>
-        </article>
+    return (
+        <MotionConfig reducedMotion='user'>
+            <article>
+                <main className="Main__Article">
+                    <motion.div className="Main__Article--div" initial={initial} whileInView={whileInView} viewport={viewport}
+                    >
+                        <h1>ELEMIKE</h1>
+                        <h2>Marzo 2024</h2>
+                        <p>Gracias a todas las personas que esperaron este momento...</p>
+                    </motion.div>
+
+                    <div className='Main__Article--About'>
+                        <motion.p initial={initial} whileInView={whileInView} viewport={viewport}
+                        >
+                            Hablemos de la sensación que sientes cuando escuchas ritmos nuevos, ritmos que te sacan de tu zona de confort pero aún así te interesan.Este será un proyecto que diariamente te recomendará canciones adecuadas a tu estilo de vida. Además, como es de costumbre en la casa, semanalmente seleccionaremos un artista en particular. Narraremos su historia, sus influencias, premios, álbumes, controversias y muchísimas cosas más.
+                        </motion.p>
+
+                        <motion.p initial={initial} whileInView={whileInView} viewport={viewport}
+                        >
+                            Me presento, mi nombre es Michael Steven Cruz Torres, más conocido como elemike. Soy estudiante de programación y mi objetivo es combinar mis dos pasiones: la carrera ya mencionada y la música. Me encantaría compartir mis conocimientos y experiencias contigo.
+                        </motion.p>
+                    </div>
+                </main>
+            </article>
+        </MotionConfig>
+
     )
 }
