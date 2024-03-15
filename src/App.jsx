ce21cd103582d6bb8c5f2a69b2bda803843c6720
childishelemike <elemike2004@gmail.com>
2024-03-15 14:54:44 -0500

    Eliminando process bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,11 @@ import person from './assets/mama.jpg'
 import { Title } from './Title.jsx'
 import { Footer } from './Footer.jsx'
 import { Copy } from './Copy.jsx'
-import { motion, useScroll } from "framer-motion";
 
 
 function App() {
-  const { scrollYProgress } = useScroll();
   return (
     <>
-      <motion.div
-        className="progress-bar"
-        style={{ scaleX: scrollYProgress }}
-      />
       <article>
         <header>
           <img className="header_image" alt='personas' src={person}></img>
